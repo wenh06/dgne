a31d26be99a2a88064ca7543dac643bfff196285
WEN Hao <wenh06@gmail.com>
2022-03-31 01:11:43 +0800

    add decorator add_docstring

--- python/utils.py
+++ python/utils.py
@@ -1,7 +1,8 @@
 """
 """
 
-from typing import List
+from functools import wraps
+from typing import List, Callable
 
 import numpy as np
 
@@ -67,3 +68,50 @@ class ReprMixin(object):
     def extra_repr_keys(self) -> List[str]:
         """ """
         return []
+
+
+def add_docstring(doc: str, mode: str = "replace") -> Callable:
+    """
+    decorator to add docstring to a function
+
+    Parameters
+    ----------
+    doc: str,
+        the docstring to be added
+    mode: str, default "replace",
+        the mode of the docstring,
+        can be "replace", "append" or "prepend",
+        case insensitive
+
+    """
+
+    def decorator(func: Callable) -> Callable:
+        """ """
+
+        @wraps(func)
+        def wrapper(*args, **kwargs) -> Callable:
+            """ """
+            return func(*args, **kwargs)
+
+        pattern = "(\s^\n){1,}"
+        if mode.lower() == "replace":
+            wrapper.__doc__ = doc
+        elif mode.lower() == "append":
+            tmp = re.sub(pattern, "", wrapper.__doc__)
+            new_lines = 1 - (len(tmp) - len(tmp.rstrip("\n")))
+            tmp = re.sub(pattern, "", doc)
+            new_lines -= len(tmp) - len(tmp.lstrip("\n"))
+            new_lines = max(0, new_lines) * "\n"
+            wrapper.__doc__ += new_lines + doc
+        elif mode.lower() == "prepend":
+            tmp = re.sub(pattern, "", doc)
+            new_lines = 1 - (len(tmp) - len(tmp.rstrip("\n")))
+            tmp = re.sub(pattern, "", wrapper.__doc__)
+            new_lines -= len(tmp) - len(tmp.lstrip("\n"))
+            new_lines = max(0, new_lines) * "\n"
+            wrapper.__doc__ = doc + new_lines + wrapper.__doc__
+        else:
+            raise ValueError(f"mode {mode} is not supported")
+        return wrapper
+
+    return decorator
